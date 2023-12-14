c5dec7050579464bfa7aef55d17513542ea6d285
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-12-13 23:49:07 -0600

    feat: agregar el menu principal en interfaz

--- main.py
+++ main.py
@@ -1,58 +1,72 @@
 import customtkinter
 from PIL import Image
 
-import passwd
-import connection_SQL
+class Menu:
+    def __init__(self,windows,user,ID):
+        self.windows = windows
+        self.user = user
+        self.ID = ID
 
-class log_in:
+        frame_Menu = customtkinter.CTkFrame(self.windows, width=100, height=430)
+        frame_Menu.place(x=10,y=10)
 
-    def __init__(self,app):
-        self.frame = customtkinter.CTkFrame(app, height=300, width=300)
-        self.frame.place(x=50, y=50)
+        img = Image.open("Image\\user-login.png")
+        render = customtkinter.CTkImage(img, size=(50, 50))
 
-        image = Image.open("Image\\user-login.png")
-        login = customtkinter.CTkImage(image, size=(100,100))
+        lbl_image = customtkinter.CTkLabel(frame_Menu, image=render, text='')
+        lbl_image.place(x=25, y=10)
+        lbl_user = customtkinter.CTkLabel(frame_Menu,text=self.user)
+        lbl_user.place(x=10,y=60)
 
-        lbl_login = customtkinter.CTkLabel(self.frame, image=login, text="")
-        lbl_login.place(x=100,y=10)
+        self.btn_employee = customtkinter.CTkButton(frame_Menu,text="Empledo", command=self.Employee, width=80,height=40)
+        self.btn_employee.place(x=10,y=100)
 
-        lbl_users = customtkinter.CTkLabel(self.frame, text="Usuario ")
-        lbl_users.place(x=50,y=130)
-        self.txt_users = customtkinter.CTkEntry(self.frame,width=200)
-        self.txt_users.place(x=50,y=160)
+        self.btn_membership = customtkinter.CTkButton(frame_Menu, text="Membresia", command=self.Membership, width=80, height=40)
+        self.btn_membership.place(x=10, y=150)
 
-        lbl_password = customtkinter.CTkLabel(self.frame, text="ID")
-        lbl_password.place(x=50,y=190)
-        self.txt_password = customtkinter.CTkEntry(self.frame, width=200)
-        self.txt_password.place(x=50,y=220)
-        self.txt_password.configure(show="*")
+        self.btn_movie = customtkinter.CTkButton(frame_Menu, text="Pelicula", command=self.Movie, width=80, height=40)
+        self.btn_movie.place(x=10, y=200)
 
-        btn_log_in = customtkinter.CTkButton(self.frame, text="Iniciar secion", command=self.log)
-        btn_log_in.place(x=75, y=260)
+        self.btn_product = customtkinter.CTkButton(frame_Menu, text="Producto", command=self.Product, width=80, height=40)
+        self.btn_product.place(x=10, y=250)
 
-        self.select_database = connection_SQL.connect_DataBase('localhost','root',passwd.passwd(),'3306','cine_paraiso')
+        self.btn_showing = customtkinter.CTkButton(frame_Menu, text="Funcion", command=self.Showing, width=80, height=40)
+        self.btn_showing.place(x=10, y=300)
+
+        self.btn_ticket = customtkinter.CTkButton(frame_Menu, text="Ticket", command=self.Ticket, width=80, height=40)
+        self.btn_ticket.place(x=10, y=350)
+
+    
+    def Employee(self):
+        pass
+
+    def Membership(self):
+        pass
+
+    def Movie(self):
+        pass
+
+    def Product(self):
+        pass
+
+    def Showing(self):
+        pass
+
+    def Ticket(self):
+        pass
         
-    def log(self):
-        user = self.select_database.Select_one('nombre', 'empleado', 'idempleado',self.txt_password.get())
-        try:
-            if (self.txt_users.get() == user[0]):
-                print("sesion iniciada")
-            else:
-                print("Nombre incorrecto")
-        except Exception as Ex:
-            print("ID incorrecto")
-
-
 if __name__ == "__main__":
+    # ----> Agregar el login
+
+    # -----------------------
+
+    name = "Ricardo"
+    ID = 1
+
     app = customtkinter.CTk()
-    app.geometry("400x400")
+    app.geometry('800x450')
+    app.title('Cinema Paraiso')
 
-    img = Image.open("Image\\Fondo.jpg")
-    render = customtkinter.CTkImage(img,size=(400,400))
-
-    lbl_image = customtkinter.CTkLabel(app, image=render, text="")
-    lbl_image.place(x=0,y=0)
-
-    log_in(app)
+    Menu(app,name,ID)
 
     app.mainloop()
