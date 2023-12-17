137a2360ee9ef8dfc4198d055140ec30688e800b
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-12-17 13:37:07 -0600

    feat: agregar interfaz de empleado

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ class Menu:
         lbl_user = customtkinter.CTkLabel(frame_Menu,text=self.user,width=80)
         lbl_user.place(x=10,y=60)
 
+        # --------> SOLO SI ES GERENTE
         self.btn_employee = customtkinter.CTkButton(frame_Menu,text="Empledo", command=self.Employee, width=80,height=40)
         self.btn_employee.place(x=10,y=100)
+        #------------------------------
 
         self.btn_membership = customtkinter.CTkButton(frame_Menu, text="Membresia", command=self.Membership, width=80, height=40)
         self.btn_membership.place(x=10, y=150)
@@ -51,6 +53,93 @@ class Menu:
     
     def Employee(self):
         self.status_btn_Menu('disabled')
+        frame_employee = customtkinter.CTkFrame(self.windows,width=670,height=430)
+        frame_employee.place(x=120,y=10)
+
+        def status_txt(status):
+            cmb_post.configure(state=status)
+            txt_rfc.configure(state=status)
+            txt_name.configure(state=status)
+            txt_email.configure(state=status)
+            txt_phone.configure(state=status)
+            txt_adress.configure(state=status)
+
+        def close():
+            frame_employee.destroy()
+            self.status_btn_Menu('normal')
+
+        def Add():
+            status_txt('normal')
+            cmb_post.set('GNL')
+
+        def Modifier():
+            pass
+
+        def Leave():
+            pass
+
+        def Save():
+            pass
+
+        def Cancel():
+            pass
+
+        lbl_id = customtkinter.CTkLabel(frame_employee,text="ID")
+        lbl_id.place(x=30, y=40)
+        txt_id = customtkinter.CTkEntry(frame_employee, width=50)
+        txt_id.place(x=50,y=40)
+
+        lbl_rfc = customtkinter.CTkLabel(frame_employee,text="RFC")
+        lbl_rfc.place(x=30, y=80)
+        txt_rfc = customtkinter.CTkEntry(frame_employee,width=140)
+        txt_rfc.place(x=80,y=80)
+
+        lbl_name = customtkinter.CTkLabel(frame_employee, text="Nombre")
+        lbl_name.place(x=250,y=80)
+        txt_name = customtkinter.CTkEntry(frame_employee,width=140)
+        txt_name.place(x=305,y=80)
+
+        lbl_email = customtkinter.CTkLabel(frame_employee,text="Correo")
+        lbl_email.place(x=30, y=120)
+        txt_email = customtkinter.CTkEntry(frame_employee, width=230)
+        txt_email.place(x=80,y=120)
+
+        lbl_phone = customtkinter.CTkLabel(frame_employee,text="Tel")
+        lbl_phone.place(x=30, y=160)
+        txt_phone = customtkinter.CTkEntry(frame_employee, width=140)
+        txt_phone.place(x=80,y=160)
+
+        lbl_adress = customtkinter.CTkLabel(frame_employee,text="Dir")
+        lbl_adress.place(x=30, y=200)
+        txt_adress = customtkinter.CTkEntry(frame_employee,width=140)
+        txt_adress.place(x=80, y=200)
+
+        lbl_post = customtkinter.CTkLabel(frame_employee,text="Cargo")
+        lbl_post.place(x=150, y=40)
+        cmb_post = customtkinter.CTkComboBox(frame_employee, values=["GRT","GNL"],width=70)
+        cmb_post.place(x=200,y=40)
+        cmb_post.set("")
+
+        btn_close = customtkinter.CTkButton(frame_employee,width=10,height=10,text="X",fg_color="RED",command=close)
+        btn_close.place(x=0,y=0)
+
+        btn_add = customtkinter.CTkButton(frame_employee, text="Agregar", width=70, height=40, fg_color="DARKBLUE", command=Add)
+        btn_add.place(x=500,y=80)
+
+        btn_modifier = customtkinter.CTkButton(frame_employee, text="Modificar", width=70, height=40, fg_color="DARKBLUE", command=Modifier)
+        btn_modifier.place(x=500,y=130)
+
+        btn_leave = customtkinter.CTkButton(frame_employee, text="Baja",width=70, height=40,fg_color="DARKBLUE", command=Leave)
+        btn_leave.place(x=500,y=180)
+
+        btn_save = customtkinter.CTkButton(frame_employee,text="Guardar",width=100,fg_color="GREEN",command=Save)
+        btn_save.place(x=290,y=160)
+
+        btn_cancel = customtkinter.CTkButton(frame_employee,text="Cancelar",width=100,fg_color="RED",command=Cancel)
+        btn_cancel.place(x=290,y=200)
+
+        txt_id.configure(state='disabled')
+        status_txt('disabled')
 
     def Membership(self):
         pass
