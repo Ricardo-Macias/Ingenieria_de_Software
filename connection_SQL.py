f83518aa06f9f2b6b5420b2a68db4a8b648b192c
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-01-04 17:58:39 -0600

    feat: agregar clase employee

--- connection_SQL.py
+++ connection_SQL.py
@@ -24,7 +24,7 @@ class connect_DataBase:
     def Select_all(self, search, table):
         try:
             cursor = self.conecction.cursor()
-            sql = "SELECT '{}' FROM '{}'".format(search, table)
+            sql = "SELECT {} FROM {}".format(search, table)
             cursor.execute(sql)
             data = cursor.fetchall()
         except Exception as Ex:
@@ -32,3 +32,39 @@ class connect_DataBase:
         finally:
             cursor.close()
             return data
+
+class employee(connect_DataBase):
+
+    def Add(self,rfc,name,email,phone,addres,post,date):
+        try:
+            id = len(self.Select_all('*','empleado')) + 1
+            cursor = self.conecction.cursor()
+            sql = f"INSERT INTO empleado(idempleado,rfc,nombre,correo,telefono,direccion,cardo,fecha_contratacion) VALUES {id},{rfc},{name},{phone},{addres},{post},{addres},{date};"
+            cursor.execute(sql)
+            self.conecction.commit()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+
+    def modifier(self, id, rfc, name, email, phone, addres, post):
+        try:
+            cursor = self.conecction.cursor()
+            sql = f"UPDATE empleado SET rfc = {rfc}, nombre = {name}, correo = {email}, telefono = {phone}, direccion = {addres}, cargo = {post} WHERE idempleado = {id};"
+            cursor.execute(sql)
+            self.conecction.commit()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+
+    def leave(self,id,date):
+        try:
+            cursor = self.conecction.cursor()
+            sql = f"UPDATE empleado SET fecha_baja WHERE idempleado = {id};"
+            cursor.execute(sql)
+            self.conecction.commit()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
