1cad16a90c05275f71bf391c00e69f02b4b31f5e
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-12-06 23:29:21 -0600

    feat: agregar funcion file_explore

--- files.py
+++ files.py
@@ -1,4 +1,5 @@
 import customtkinter
+from tkinter import filedialog
 from PIL import Image
 
 def open_image(img, render=()):
@@ -7,3 +8,7 @@ def open_image(img, render=()):
         return customtkinter.CTkImage(img, size=render)
     else:
         return img
+    
+def file_explore():
+    poster = filedialog.askopenfilename(title="Buscar Poster",filetypes=[("Archivos de imagen",("*.jpg","*.png","*.jpeg"))])
+    return poster
