eb7d9f7bb2a6fe8b8f7b5ae3269a7540bec8ca24
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-12-07 00:16:47 -0600

    feat: agregar set y get de file_explore

--- files.py
+++ files.py
@@ -2,13 +2,19 @@ import customtkinter
 from tkinter import filedialog
 from PIL import Image
 
-def open_image(img, render=()):
-    img = Image.open(img)
-    if (render):
-        return customtkinter.CTkImage(img, size=render)
-    else:
-        return img
+class files:
+    def __init__(self):
+        self.poster = None
+
+    def open_image(self,img, render=()):
+        img = Image.open(img)
+        if (render):
+            return customtkinter.CTkImage(img, size=render)
+        else:
+            return img
+        
+    def setfile_explore(self):
+        self.poster = filedialog.askopenfilename(title="Buscar Poster",filetypes=[("Archivos de imagen",("*.jpg","*.png","*.jpeg"))])
     
-def file_explore():
-    poster = filedialog.askopenfilename(title="Buscar Poster",filetypes=[("Archivos de imagen",("*.jpg","*.png","*.jpeg"))])
-    return poster
+    def getfile_explore(self):
+        return self.poster
