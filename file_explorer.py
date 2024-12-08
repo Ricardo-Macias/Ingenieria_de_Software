d9f1d44d8d8d485e3432da9bea14a83e2ec8f3e2
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-12-07 19:17:52 -0600

    refactor: cambiar nombre de files a file_explorer

---
+++ file_explorer.py
@@ -0,0 +1,20 @@
+import customtkinter
+from tkinter import filedialog
+from PIL import Image
+
+class image_explorer:
+    def __init__(self):
+        self.poster = None
+
+    def open_image(self,img, render=()):
+        img = Image.open(img)
+        if (render):
+            return customtkinter.CTkImage(img, size=render)
+        else:
+            return img
+        
+    def setimage_explorer(self):
+        self.poster = filedialog.askopenfilename(title="Buscar Poster",filetypes=[("Archivos de imagen",("*.jpg","*.png","*.jpeg"))])
+    
+    def getimage_explorer(self):
+        return self.poster
