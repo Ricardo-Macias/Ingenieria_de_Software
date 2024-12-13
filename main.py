58255ea2fbf18c318b2769fa402f25da0cbae11c
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2024-12-12 23:27:46 -0600

    feat: Agregar funcion Showing

--- main.py
+++ main.py
@@ -700,7 +700,155 @@ class Menu:
         table()
         
     def Showing(self):
-        pass
+        self.status_btn_Menu('disabled')
+        frame_showing = customtkinter.CTkFrame(self.windows, width=600, height=430)
+        frame_showing.place(x=120, y=10)
+
+        frame_button = customtkinter.CTkFrame(frame_showing, width=580, height=80)
+        frame_button.place(x=10, y=25)
+
+        def table():
+            global frame_table, table_showing
+
+            frame_table = customtkinter.CTkFrame(frame_showing, width=580, height=300)
+            frame_table.place(x=10, y=120)
+
+            table_showing = ttk.Treeview(frame_table, columns=('col1', 'col2', 'col3' ,'col4'))
+            table_showing.column('#0', width=50, anchor=customtkinter.CENTER)
+            table_showing.column('col1', width=140, anchor=customtkinter.CENTER)
+            table_showing.column('col2', width=120, anchor=customtkinter.CENTER)
+            table_showing.column('col3', width=120, anchor=customtkinter.CENTER)
+            table_showing.column('col4', width=120, anchor=customtkinter.CENTER)
+
+            table_showing.heading('#0', text='ID')
+            table_showing.heading('col1', text='Pelicula')
+            table_showing.heading('col2', text='Sala')
+            table_showing.heading('col3', text='Fecha y Hora')
+            table_showing.heading('col4', text='Precio')
+
+            table_showing.place(x=30, y=30, width=810, height=400)
+
+            add_table()
+
+        def form(id, option):
+            global txt_name, txt_price, txt_stock, frame_form
+
+            frame_table.destroy()
+
+            frame_form = customtkinter.CTkFrame(
+                frame_showing, width=580, height=300)
+            frame_form.place(x=10, y=120)
+
+            lbl_form = customtkinter.CTkLabel(
+                frame_form, text=f"{option}", font=self.font_title)
+            lbl_form.place(x=50, y=10)
+
+            lbl_id = customtkinter.CTkLabel(
+                frame_form, text=f"ID: {id}", font=self.font_id)
+            lbl_id.place(x=50, y=50)
+
+            lbl_movie = customtkinter.CTkLabel(frame_form, text="Pelicula")
+            lbl_movie.place(x=50, y=85)
+            txt_movie = customtkinter.CTkEntry(frame_form, width=240)
+            txt_movie.place(x=110, y=85)
+
+            lbl_cinema_room= customtkinter.CTkLabel(frame_form, text="Sala")
+            lbl_cinema_room.place(x=50, y=120)
+            txt_cinema_room = customtkinter.CTkEntry(frame_form, width=100)
+            txt_cinema_room.place(x=110, y=120)
+
+            lbl_date = customtkinter.CTkLabel(frame_form, text="Fecha")
+            lbl_date.place(x=50, y=155)
+            txt_date = customtkinter.CTkEntry(frame_form, width=100)
+            txt_date.place(x=110, y=155)
+
+            lbl_price = customtkinter.CTkLabel(frame_form, text="Precio")
+            lbl_price.place(x=50, y=190)
+            txt_price = customtkinter.CTkEntry(frame_form, width=100)
+            txt_price.place(x=110, y=190)
+
+            btn_save = customtkinter.CTkButton(
+                frame_form, text="Guardar", width=100, fg_color="GREEN", command=lambda: Save(id, option))
+            btn_save.place(x=140, y=240)
+
+            btn_cancel = customtkinter.CTkButton(
+                frame_form, text="Cancelar", width=100, fg_color="RED", command=Cancel)
+            btn_cancel.place(x=260, y=240)
+
+            self.status_btn_add_modifier_delete('disabled')
+
+        def add_table():
+            product = self.product_sql.Select_all('*', 'funcion')
+            for count in product:
+                table_showing.insert("", customtkinter.END, text=count[0], values=[
+                    count[1], count[2], count[3]])
+
+        def close():
+            frame_showing.destroy()
+            self.status_btn_Menu('normal')
+
+        def Add():
+            id = self.product_sql.last_id('idfuncion', 'funcion') + 1
+            form(id, 'Agregar')
+
+        def Modifier():
+            select = table_showing.focus()
+            key = table_showing.item(select, 'text')
+
+            if key == "":
+                messagebox.showwarning("Modificar", "Selecciona un elemento")
+            else:
+                value = table_showing.item(select, 'values')
+                form(key, 'Modificar')
+
+                txt_name.insert(0, value[0])
+                txt_price.insert(0, value[1])
+                txt_stock.insert(0, value[2])
+
+        def Leave():
+            select = table_showing.focus()
+            key = table_showing.item(select, 'text')
+
+            if key == "":
+                messagebox.showwarning("Baja", "Selecciona un elemento")
+            else:
+                value = table_Product.item(select, 'values')
+                option = messagebox.askquestion(
+                    'Baja', f'Dar de baja a {value[0]}')
+                if option == 'yes':
+                    self.product_sql.Delete(key)
+                    table_Product.destroy()
+                    table()
+
+        def Save(id, option):
+            if option == 'Agregar':
+                self.product_sql.Add(id, txt_name.get(),
+                                     txt_price.get(), txt_stock.get())
+                messagebox.showinfo("Agregar", "Nuevo membresia agregada")
+            else:
+                self.product_sql.Modifier(
+                    id, txt_name.get(), txt_price.get(), txt_stock.get())
+                messagebox.showinfo(
+                    'Modificar', 'Se modificaron los datos de la membresia')
+            frame_form.destroy()
+            self.status_btn_add_modifier_delete('normal')
+            table()
+
+        def Cancel():
+            option = messagebox.askokcancel(
+                'Cancelar', 'Seguro que quiere cancelar')
+            if option:
+                frame_form.destroy()
+                self.status_btn_add_modifier_delete('normal')
+                table()
+
+        btn_close = customtkinter.CTkButton(
+            frame_showing, width=10, height=10, text="X", fg_color="RED", command=close)
+        btn_close.place(x=0, y=0)
+
+        self.create_btn_add_edit_delete(
+            frame_button, "Funciones", Add, Modifier, Leave)
+        table()
 
     def Ticket(self):
         pass
